35424e7b13b7cb73746b0e1a555930b53a477d46
DiogoOnze <diogo@konakona.moe>
2022-12-12 18:12:45 +0100

    config laptop (think420)

--- config.h
+++ config.h
@@ -64,9 +64,9 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format                 argument */
-	{ netspeed_rx,  "[%sB/s ",	       "wlan0" },
+	{ netspeed_rx,  "[%sB/s ",	       "wlan0" },
 	{ netspeed_tx,  "%sB/s ",	       "wlan0" },
-	{ wifi_perc,    "%s%%] ",             "wlan0" },	
+	{ wifi_perc,    "%s%%] ",             "wlan0" },	
 	{ cpu_perc,     "[%s%% ",                NULL },
         { run_command,  "%s] ",             "sensors | awk '/^Package/ {print $4}' | sed 's/+//' | sed 's/.0//;s/.0//;s/°//'" }, 
 	{ ram_used,     "[%sB ",                 NULL },
